b27459eaa51f95de7c5a8be96c0e6d9613a343e6
kuini <39581685+kuini-1@users.noreply.github.com>
2025-01-05 20:53:29 +0700

    Update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -100,7 +100,7 @@ export default defineNuxtConfig({
   css: [
     'primeicons/primeicons.css',
     '@sfxcode/formkit-primevue/dist/sass/formkit-primevue.scss',
-    '@/assets/app.css',
+    '~_nuxt/assets/app.css',
   ],
 
   build: {
